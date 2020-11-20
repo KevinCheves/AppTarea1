c454abf4c108229e6ca861f5cab8b9afd2db65de
KevinCheves <kevin.cheves2016@uteq.edu.ec>
2020-11-20 18:11:29 -0500

    commit one

--- app/src/main/java/com/appmovil/apptarea1/MainActivity.java
+++ app/src/main/java/com/appmovil/apptarea1/MainActivity.java
@@ -2,7 +2,10 @@ package com.appmovil.apptarea1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+    public void btnEnviar(View view)
+    {
+        //Creamos el Intent
+        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+        EditText txtNombre = (EditText)findViewById(R.id.txtNombre);
+        //Creamos la información a pasar entre actividades - Pares Key-Value     
+        Bundle b = new Bundle();      
+        b.putString("NOMBRE", txtNombre.getText().toString());     
+        //Añadimos la información al intent      
+        intent.putExtras(b);     
+        // Iniciamos la nueva actividad      
+        startActivity(intent);
+    }
+
+    }
