247c4a12256195105acd4ca3874a0d5427318708
KevinCheves <kevin.cheves2016@uteq.edu.ec>
2020-11-22 01:20:16 -0500

    commit fecha

--- app/src/main/java/com/appmovil/apptarea1/MainActivity.java
+++ app/src/main/java/com/appmovil/apptarea1/MainActivity.java
@@ -2,21 +2,32 @@ package com.appmovil.apptarea1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
 import com.google.android.material.snackbar.Snackbar;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Calendar;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText txtFecha;
+    Button btnFecha;
+    private int dia, mes, year;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtFecha = (EditText)findViewById(R.id.txtFecha);
+        btnFecha = (Button)findViewById(R.id.btnFecha);
+        btnFecha.setOnClickListener(this);
     }
     public void btnEnviar(View view)
     {
@@ -52,5 +63,27 @@ public class MainActivity extends AppCompatActivity {
             Snackbar.make(view,"llene todos los campos",Snackbar.LENGTH_SHORT).show();
         }
     }
+    public void cargarFecha(View view)
+    {
 
     }
+
+    @Override
+    public void onClick(View v) {
+        if(v==btnFecha){
+            final Calendar c = Calendar.getInstance();
+            dia =  c.get(Calendar.DAY_OF_MONTH);
+            mes = c.get(Calendar.MONTH);
+            year = c.get(Calendar.YEAR);
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                    txtFecha.setText(dayOfMonth+"/"+(monthOfYear+1)+"/"+year);
+                }
+            }
+                    ,dia,mes,year);
+            datePickerDialog.show();
+        }
+    }
+}
